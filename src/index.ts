2f4611ff159f1dc26349bed2f1d7c8d9d8e9a7ed
hrmq <h.rahimi.moqadam@gmail.com>
2020-04-16 23:19:07 -0400

    add express middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hi there');
+import express from 'express';
+import http from 'http';
+import path from 'path';
+
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+
+const app: express.Application = express();
+
+// Express.js configuration
+app.set('port', process.env.port || 3000);
+
+app.use(bodyParser.json({ limit: '100kb' }));
+app.use(logger('dev'));
+
+app.get('/', (req, res) => {
+  res.send('hi there');
+});
+
+const server = http.createServer(app);
+
+server.listen(app.get('port'), function () {
+  console.log('server is running');
+});
